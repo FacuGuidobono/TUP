1f7d03f87642baea21c60b9e5efedc7be6a7b700
Facu Guidobono <facundo.guidobono@gmail.com>
2024-07-18 13:56:58 -0300

    Update

---
+++ PROGRAMACION_INICIAL/S3/Integrador_ES.py
@@ -0,0 +1,47 @@
+'''
+🔔Integrador !
+La juguetería El MUNDO DE CHARLY nos encarga un programa para conocer qué cantidad de materiales se necesita para la fabricación de distintos juguetes.
+
+#*CONFECCIÓN DE UN COMETA: 
+
+#!          B
+#!          ^
+#!        / | \
+#!      /   |   \
+#*     D---------C
+#?      \   |   /
+#?       \  |  /
+#?        \ | /
+#?         \|/
+#?          v
+#?          A
+
+
+#!Medidas:
+
+#* AB = Diágonal mayor
+
+#* DC = Diágonal menor
+
+#! BD y BC = lados menores
+
+#? AD y AC = lados mayores
+
+#! El usuario ingresará las medidas  BC, CD y DA.
+
+Detalles de construcción
+
+Debemos tener en cuenta que la estructura del cometa estará dada por un perímetro de varillas de plástico y los correspondientes entrecruces (DC y AB) del mismo material para mantener la forma del cometa.
+
+El cometa estará construido con papel de alta resistencia. La cola del mismo se construirá con el mismo papel que el cuerpo y representará un 10% adicional del necesario para el cuerpo.
+
+Necesitamos saber cuántos Mts de varillas de plástico y cuántos de papel son necesarios para la construcción en masa de 10 cometas. Tener en cuenta que los valores de entrada están expresados en Cms.
+@FacuGuidobono
+'''
+
+print('-'*100)
+print('JUGUETERIA EL MUNDO DE CHARLY'.center(80))
+print('-'*100)
+
+
+
